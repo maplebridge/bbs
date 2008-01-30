01cb51459e68307269344e896596c6f56c3d4f9d
ckm <ckm.bbs@bbs.cs.nthu.edu.tw>
2008-01-30 11:06:16 +0000

    add showACCTlist for showing user data list
    git-svn-id: http://bbs.cs.nthu.edu.tw/svn/maplebbs/trunk@56 bd1103fd-f9b1-db11-a66c-000d613fc1d9

---
+++ util/showACCTlist.c
@@ -0,0 +1,59 @@
+/*-------------------------------------------------------*/
+/* util/showACCTlist.c  ( NTHU CS MapleBBS Ver 3.10 )    */
+/*-------------------------------------------------------*/
+/* target : 顯示使用者資料列表 (Modified from showACCT.c)   */
+/* create : 08/01/30                                     */
+/* update :                                              */
+/* author : ckm.bbs@bbs.cs.nthu.edu.tw                   */
+/*-------------------------------------------------------*/
+
+
+#include "bbs.h"
+
+int
+main(argc, argv)
+  int argc;
+  char **argv;
+{
+  int c;
+  
+  printf("%-15s %-60s\n", "ID", "Email");
+  printf("> ------------------------------------------------------------------------------------------ \n");
+  
+  for (c = 'a'; c <= 'z'; c++)
+  {
+    char buf[64];
+    struct dirent *de;
+    DIR *dirp;
+
+    sprintf(buf, BBSHOME "/usr/%c", c);
+    chdir(buf);
+
+    if (!(dirp = opendir(".")))
+      continue;
+
+    while (de = readdir(dirp))
+    {
+      ACCT acct;
+      int fd;
+      char *str;
+
+      str = de->d_name;
+      if (*str <= ' ' || *str == '.')
+        continue;
+
+      if ((argc == 2) && str_cmp(str, argv[1]))
+        continue;
+      
+      sprintf(buf, "%s/" FN_ACCT, str);
+      if (rec_get(fpath, &acct, sizeof(ACCT), 0) < 0)
+      {
+        printf("%s: read error (maybe no such id?)\n", str);
+        continue;
+      }
+      
+      printf("%-15s %-60s\n", acct->userid, acct->email);
+
+ 
+  return 0;
+}
